2ab3138bfb33511002595de1c64d92b2a5d4e2ca
Marcos Gonzalvez <marcosgondesign@gmail.com>
2024-08-04 20:19:25 -0300

    agregando el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './componentes/Navbar'
+import Home from './componentes/Home'
+import Footer from './componentes/Footer'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  )
+}
+
+export default App
